af358d7c5fff4e440714689256f17cb81e7892ee
lowtierbot <scrublord@kek.com>
2023-07-24 20:31:14 +0200

    print string and char

--- 0-print_c_s.c
+++ 0-print_c_s.c
@@ -1,129 +1,60 @@
+#include <stdarg.h>
 #include <unistd.h>
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
 /**
-* _printf - print charcters and strings and integers
-* @format: string of char that may contain %c or %s
-* @...: variadic args
-* Return: number of printed chars
+* _printf - print chars and strings
+* @format: string
+* @...: variadic
+* Return: number of printed characters
 */
 int _printf(const char *format, ...)
 {
-	char *to_print, *argu;
-	int i = 0;
-	int len;
+	int i = 0, cnt = 0;
 	va_list args;
+	char c;
 
-	to_print = "\0";
 	va_start(args, format);
-	to_print = _strcpy(to_print, format);
-	len = _strlen(to_print);
-	for (; i < len ; i++)
+	while (format[i] != '\0')
 	{
-		if (to_print[i] == '%')
+		if (format[i] == '%')
 		{
-			if (to_print[i + 1] == 's')
+			if (format[i + 1] == 's')
 			{
-				argu  = va_arg(args, char *);
-				to_print = insert(to_print, argu, i);
-				i += _strlen(argu);
-				len = _strlen(to_print);
+				cnt += print_string(va_arg(args, char *));
+				i++;
 			}
-			else if (to_print[i + 1] == 'c')
+			else if (format[i + 1] == 'c')
 			{
-				argu = char_to_string(argu, va_arg(args, int));
-				to_print = insert(to_print, argu, i);
-				i += _strlen(argu);
-				len = _strlen(to_print);
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				cnt++;
+				i++;
 			}
-			else
-				return (0);
 		}
+		else
+		{
+			write(1, &format[i], 1);
+			cnt++;
+		}
+		i++;
 	}
-	va_end(args);
-	len = _strlen(to_print);
-	write(1, to_print, len);
-/*
-*	free(to_print);
-*	free(argu);
-*/
-	return (len);
-}
-/**
-* _strlen - count number of chars
-* @s: the string to count
-* Return: int
-*/
-unsigned int _strlen(const char *s)
-{
-	unsigned int cnt = 0;
-
-	for (; s[cnt] != '\0'; cnt++)
-		;
 	return (cnt);
 }
 /**
-* _strcpy - copy string intro another
-* @dest: destination
-* @src: source
-* Return: pointer to dest (important due to malloc that can change pointer)
+* print_string - print a string xD
+* @s: the string
+* Return: number of char printed
 */
-char *_strcpy(char *dest, const char *src)
+int print_string(const char *s)
 {
-	int len, i = 0;
+	int i = 0;
 
-	len = _strlen(src);
-	dest = malloc((len + 1) * sizeof(char));
-	if (dest == NULL)
-	{
-		free(dest);
-		return (NULL);
-	}
-	for (; i <= len; i++)
-		dest[i] = src[i];
-	return (dest);
-}
-/**
-* insert - insert 'to_insert" inside dest in position n and skipin n, n+1 (%d)
-* @dest: destination
-* @to_insert: string to insert
-* @n: position of insertion
-* Return: pointer to destination
-*/
-char *insert(char *dest, char *to_insert, int n)
-{
-	unsigned int i, j = 0;
-	char *temp_string;
-	int len;
-
-	len = _strlen(dest) + _strlen(to_insert);
-	temp_string = malloc(len * sizeof(char));
-	temp_string = _strcpy(temp_string, dest);
-	for (i = 0; i < _strlen(to_insert); i++)
-		temp_string[i + n] = to_insert[i];
-	do {
-		temp_string[i + n + j] = dest[n + 2 + j];
-		j++;
-	} while (dest[n + 2 + j] != '\0');
-	return (temp_string);
-}
-/**
-* char_to_string - returns string containing the char
-* @s: ptr where to store the string
-* @c: character
-* Return: a string
-*/
-char *char_to_string(char *s, char c)
-{
-	s = malloc(2 * sizeof(char));
 	if (s == NULL)
+		return (0);
+	while (s[i] != '\0')
 	{
-		free(s);
-		return (NULL);
+		write(1, &s[i], 1);
+		i++;
 	}
-	s[0] = c;
-	s[1] = '\0';
-	return (s);
+	return (i);
 }
--- main.h
+++ main.h
@@ -1,8 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _printf(const char *, ...);
-unsigned int _strlen(const char *);
-char *_strcpy(char *, const char *);
-char *insert(char *dest, char *to_insert, int n);
-char *char_to_string(char * ,char);
+int print_string(const char *);
 #endif
