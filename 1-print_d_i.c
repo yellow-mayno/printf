fa6988489cd7cf6f96877d6ed3676e824b952d68
lowtierbot <scrublord@kek.com>
2023-07-25 23:40:34 +0200

    auto push

--- 1-print_d_i.c
+++ 1-print_d_i.c
@@ -8,21 +8,20 @@
 int print_number(int num)
 {
 	int power(int, int);
-	int i = 0;
+	int i = 0, cnt = 0;
 	int n = num;
 	char c;
 
 	if (n < 0)
 	{
 		c = '-';
-		write(1, &c, 1);
+		cnt += write(1, &c, 1);
 	}
 	while (num / 10)
 	{
 		i++;
 		num = num / 10;
 	}
-	num = i;
 	for (; i >= 0; i--)
 	{
 		if (n < 0)
@@ -30,9 +29,9 @@ int print_number(int num)
 		else
 			c = n / power(10, i) + 48;
 		n = n % power(10, i);
-		write(1, &c, 1);
+		cnt += write(1, &c, 1);
 	}
-	return (num + 1);
+	return (cnt);
 }
 /**
 * power - power xD
@@ -48,4 +47,38 @@ int power(int num, int pow)
 	for (; i < pow; i++)
 		cml = cml * num;
 	return (cml);
+}
+/**
+* print_number_binary - as the name says xDD
+* @num: number to convert
+* Return: string
+*/
+int print_number_binary(int num)
+{
+	int power(int, int);
+	int i = 0;
+	int n = num;
+	char c;
+
+	if (n < 0)
+	{
+		c = '-';
+		write(1, &c, 1);
+	}
+	while (num / 2)
+	{
+		i++;
+		num = num / 2;
+	}
+	num = i;
+	for (; i >= 0; i--)
+	{
+		if (n < 0)
+			c = n / power(2, i) * (-1) + 48;
+		else
+			c = n / power(2, i) + 48;
+		n = n % power(2, i);
+		write(1, &c, 1);
+	}
+	return (num + 1);
 }
--- main.h
+++ main.h
@@ -3,5 +3,6 @@
 int _printf(const char *, ...);
 int print_string(const char *);
 int power(int, int);
+int print_number_binary(int);
 int print_number(int);
 #endif
